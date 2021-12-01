b3a51f42aac75fcb1627b9b44ef80f306e0930fb
DESKTOP-HDMAUKS\wlstn <mftqn555@gmail.com>
2021-12-01 21:42:42 +0900

    자판기 프로그램 1차 수정
    변수명 수정, 잔돈 반환 기능 추가,  금액 예외처리 추가

--- src/vendingmachine/VendingMachine.java
+++ src/vendingmachine/VendingMachine.java
@@ -6,6 +6,9 @@ public class VendingMachine {
     int priceBeverage2 = 1000;
     int priceBeverage3 = 800;
     int priceBeverage4 = 900;
+    int inputMoney;
+    int price;
+    int balance;
 
     String nameBeverage1 = "콜라";
     String nameBeverage2 = "오렌지";
@@ -14,54 +17,87 @@ public class VendingMachine {
 
     public boolean run = true;
 
-    int displayMoney = 0;
-    int price = 0;
 
     private static void log(String message){
         System.out.println(message);
     }
 
     public void displayOption(){
-      log("=======================================");
-      log("  1. 돈 넣기 | 2. 잔액 반환, 종료 " );
-      log("=======================================");
+        log("===================================");
+        log(" 1. 돈 넣기 | 2. 잔액 반환, 종료 " );
+        log("====================================");
+        log("잔액: " + balance);
+        log("옵션을 선택하세요 >> ");
     }
 
-    public void pushOptionButton(int buttonNum){
-        switch (buttonNum) {
+    public void selectOption(int selectNum){
+        switch (selectNum) {
             case 1:
                 break;
             case 2:
-                log("잔액이 반환 되었습니다");
+                returnBalance();
                 System.exit(0);
         }
     }
 
-
     public void displayMenu(){
+        log("====================================");
         log("1 "+nameBeverage1+"("+priceBeverage1+")   2 "+nameBeverage2+"("+priceBeverage2+")");
         log("3 "+nameBeverage3+"("+priceBeverage3+")   4 "+nameBeverage4+"("+priceBeverage4+")");
+        log("====================================");
+        log("잔액: " + balance);
+        log("돈을 투입하세요 >>");
     }
 
     public void pushMenuButton(int buttonNum){
         switch (buttonNum){
             case 1:
                 price = priceBeverage1;
-                log(nameBeverage1+"가 나왔습니다");
                 break;
             case 2:
                 price = priceBeverage2;
-                log(nameBeverage2+"가 나왔습니다");
                 break;
-            case 3: 
+            case 3:
                 price = priceBeverage3;
-                log(nameBeverage3+"가 나왔습니다");
                 break;
             case 4:
                 price = priceBeverage4;
-                log(nameBeverage4+"가 나왔습니다");
                 break;
         }
     }
 
+    public void giveBeverage() {
+        if(price > balance) {
+            balance = inputMoney;
+            log("잔액이 부족합니다.");
+        } else {
+            if (price == priceBeverage1) {
+                log(nameBeverage1 + "가 나왔습니다");
+            } else if (price == priceBeverage2) {
+                log(nameBeverage2 + "가 나왔습니다");
+            } else if (price == priceBeverage3) {
+                log(nameBeverage3 + "가 나왔습니다");
+            } else if (price == priceBeverage4) {
+                log(nameBeverage4 + "가 나왔습니다");
+            }
+        }
+    }
+
+    public void calculateBalance() {
+        balance = balance - price;
+        balance = balance < 0 ? inputMoney : balance;
+    }
+
+    public void returnBalance() {
+        int coin500 = (balance / 500);
+        int coin100 = (balance % 500) / 100;
+        int coin50 = (balance % 500 % 100) / 50;
+        int coin10 = (balance % 500 % 100 % 50) / 10;
+
+        log("500원 " + coin500 + "개");
+        log("100원 " + coin100 + "개");
+        log("50원 " + coin50 + "개");
+        log("10원 " + coin10 + "개 반환 되었습니다.");
+
+    }
 }
